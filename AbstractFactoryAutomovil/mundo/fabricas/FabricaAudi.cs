089de22b35988961738a0b4b6f90ae9f96d03d28
jjmebur@gmail.com <jjmebur@gmail.com>
2014-09-28 19:52:14 -0500

    repositorio de fabrica de autos Jhon Mera

---
+++ AbstractFactoryAutomovil/mundo/fabricas/FabricaAudi.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AbstractFactoryAutomovil.mundo.interfaces;
+using AbstractFactoryAutomovil.mundo.clases;
+
+namespace AbstractFactoryAutomovil.mundo.fabricas
+{
+    class FabricaAudi : IFabricaDePartes
+    {
+
+        public IAuto crearAuto()
+        {
+            return new AutoAudi();
+        }
+
+        public IMotor crearMotor()
+        {
+            return new MotorAudi();
+        }
+
+        public ICarroceria crearCarroceria()
+        {
+            return new CarroceriaAudi();
+        }
+    }
+}
